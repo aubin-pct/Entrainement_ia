81ffec61a5b66bc99a044e96a7807aaeb8abedc4
percheta <aubin.perchet@etu.umontpellier.fr>
2025-01-20 16:51:36 +0100

    dessin fonctionnel

--- rendue_1/app.py
+++ rendue_1/app.py
@@ -1,9 +1,31 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk
 
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-root.mainloop()     
+"Commence à dessiner une ligne à partir de la position de la souris."
+def start_drawing(event):
+    global last_x, last_y
+    last_x, last_y = event.x, event.y
+
+"Dessine une ligne de la dernière position à la position actuelle de la souris."
+def draw(event):
+    global last_x, last_y
+    canvas.create_line(last_x, last_y, event.x, event.y, width=4, fill='black', capstyle=tk.ROUND, smooth=tk.TRUE)
+    last_x, last_y = event.x, event.y
+
+"Arrête le dessin."
+def stop_drawing(event):
+    global last_x, last_y
+    last_x, last_y = None, None
+
+root = tk.Tk()
+root.title("Dessin avec la souris")
+
+canvas = tk.Canvas(root, width=600, height=400, bg='white')
+canvas.pack()
+
+canvas.bind('<ButtonPress-1>', start_drawing)
+canvas.bind('<B1-Motion>', draw)
+canvas.bind('<ButtonRelease-1>', stop_drawing)
+
+
+
+root.mainloop()
