785760bfe5af9ddc541d6142392550ecef9e13f2
percheta <aubin.perchet@etu.umontpellier.fr>
2025-01-21 10:23:16 +0100

    mise en classe

--- rendue_1/app.py
+++ rendue_1/app.py
@@ -1,31 +1,11 @@
+from reconnaissance_chiffre import Draw
 import tkinter as tk
 
-"Commence à dessiner une ligne à partir de la position de la souris."
-def start_drawing(event):
-    global last_x, last_y
-    last_x, last_y = event.x, event.y
-
-"Dessine une ligne de la dernière position à la position actuelle de la souris."
-def draw(event):
-    global last_x, last_y
-    canvas.create_line(last_x, last_y, event.x, event.y, width=4, fill='black', capstyle=tk.ROUND, smooth=tk.TRUE)
-    last_x, last_y = event.x, event.y
-
-"Arrête le dessin."
-def stop_drawing(event):
-    global last_x, last_y
-    last_x, last_y = None, None
 
 root = tk.Tk()
 root.title("Dessin avec la souris")
 
-canvas = tk.Canvas(root, width=600, height=400, bg='white')
-canvas.pack()
-
-canvas.bind('<ButtonPress-1>', start_drawing)
-canvas.bind('<B1-Motion>', draw)
-canvas.bind('<ButtonRelease-1>', stop_drawing)
-
+draw = Draw.Draw(root=root)
 
 
 root.mainloop()
---
+++ rendue_1/reconnaissance_chiffre/Draw.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+from PIL import Image # Pour la sauvegarde de l'image
+import numpy as np   
+
+class Draw:
+    global last_x, last_y
+    def __init__(self, root, width=600, height=400):
+        self.canvas = tk.Canvas(root, width=width, height=height, bg='white')
+        self.canvas.pack()
+        self.canvas.bind('<ButtonPress-1>', self.start_drawing)
+        self.canvas.bind('<B1-Motion>', self.draw)
+        self.canvas.bind('<ButtonRelease-1>', self.stop_drawing)
+        self.last_x, self.last_y = None, None
+
+
+    # Commence à dessiner une ligne à partir de la position de la souris.
+    def start_drawing(self, event):
+        self.last_x, self.last_y = event.x, event.y
+
+    # Dessine une ligne de la dernière position à la position actuelle de la souris.
+    def draw(self, event):
+        self.canvas.create_line(self.last_x, self.last_y, event.x, event.y, width=4, fill='black', capstyle=tk.ROUND, smooth=tk.TRUE)
+        self.last_x, self.last_y = event.x, event.y
+
+    # Arrête le dessin.
+    def stop_drawing(self, event):
+        self.last_x, self.last_y = None, None
+
+    # Arrête le dessin.
+    def drawing_to_matrix():
+        pass
+   
+
