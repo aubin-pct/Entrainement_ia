fea27c1964ecf030e4c83bd7ee7416fbc890bac7
percheta <aubin.perchet@etu.umontpellier.fr>
2025-01-29 10:21:45 +0100

    add cross validation

--- rendu_3/app.py
+++ rendu_3/app.py
@@ -1,12 +1,13 @@
 import numpy as np
 import pandas as pd
+import test
 import utile.LinearRegression as l
 import utile.PolynomialRegression as p
 import utile.Scaler as Scaler
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import mean_squared_error, r2_score
-
+from sklearn.model_selection import KFold
 
 
 
@@ -18,16 +19,21 @@ from sklearn.metrics import mean_squared_error, r2_score
 # Extraction
 df_csv = pd.read_csv("rendu_3/csv_files/Position_Salaries.csv")
 
+
 # Mise en forme, à modifier selon le fichier donné
 x_train = df_csv.iloc[:, 1].values.reshape(-1,1)
 y_train = df_csv.iloc[:, -1].values.reshape(-1,1)
 
+kf = KFold(n_splits=10, shuffle=True, random_state=42)
+
+
 mse = np.array([])
+mse_mean = np.array([])
 r2 = np.array([])
 
 # Graphique des ordre 1 à 6
 plt.scatter(x_train, y_train, label="Données réelles", color='black')
-for degree in range(1,6):
+for degree in range(1,len(x_train)):
     # Création de la transformation polynomiale
     poly_reg = PolynomialFeatures(degree=degree)
     X_poly = poly_reg.fit_transform(x_train)
@@ -51,13 +57,23 @@ for degree in range(1,6):
     
     # Tracer la courbe pour ce degré
     plt.plot(X_grid, y_grid_pred, label=f"Degré {degree}")
+    mse_temp = np.array([])
+    for train_index, test_index in kf.split(x_train):
+        X_Train, X_Test = X_poly[train_index], X_poly[test_index]
+        Y_Train, Y_Test = y_train[train_index], y_train[test_index]
+
+        lin_reg.fit(X_Train, Y_Train)  # Entraînement du modèle
+        y_pred = lin_reg.predict(X_Test) 
+        mse_temp = np.append(mse_temp, lin_reg.MSE(Y_Test, y_pred))
+    mse_mean = np.append(mse_mean, np.mean(mse_temp))
 plt.xlabel("X")
 plt.ylabel("Y")
 plt.legend()
 plt.show()
 
 # Graphique mean squared error
-plt.scatter(range(1,6), mse, label="MSE", color='black')
+plt.scatter(range(1,len(x_train)), mse, label="MSE", color='black')
+plt.scatter(range(1,len(x_train)), mse_mean, label="MSE Cross Validation test", color='red')
 plt.title("Régression Polynomial : Evolution de l'erreur quadratique moyenne")
 plt.xlabel("Ordre")
 plt.ylabel("Y")
@@ -65,7 +81,7 @@ plt.legend()
 plt.show()
 
 # Graphique coefficient de détermination
-plt.scatter(range(1,6), r2, label="R2", color='blue')
+plt.scatter(range(1,len(x_train)), r2, label="R2", color='blue')
 plt.title('Régression Polynomial : Evolution du coefficient de détermination')
 plt.xlabel("Ordre")
 plt.ylabel("Y")
