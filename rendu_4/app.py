443c41f19163579d8617e9c8d4f257d369c8af87
percheta <aubin.perchet@etu.umontpellier.fr>
2025-01-30 14:51:47 +0100

    adaption dataset

--- rendu_4/app.py
+++ rendu_4/app.py
@@ -18,15 +18,28 @@ plt.show()
 
 min_max_scaler = preprocessing.MinMaxScaler()
 # Liste des relations à tracer
-relations = [
-    (['INDUS', 'NOX', 'AGE'], 'DIS'),
-    (['LSTAT'], 'MEDV'),
-    (['INDUS', 'AGE'], 'NOX'),
-    (['RAD'], 'TAX')
-]
 
+high_corr = corr_matrix[abs(corr_matrix) > 0.7].stack().reset_index()
+high_corr = high_corr[high_corr['level_0'] != high_corr['level_1']] 
+high_corr = high_corr[high_corr.apply(lambda x: x['level_0'] < x['level_1'], axis=1)]
+
+print(high_corr)
+
+relations = []
+temp = None
+tab = []
+for _, serie in high_corr.iterrows():
+    if (temp == None):
+        temp = serie["level_0"]
+    elif (temp != serie["level_0"]):
+        relations.append((tab, temp))
+        temp = serie["level_0"]
+        tab = []
+    tab.append(serie["level_1"])
+
+nb_row = len(high_corr) // 4
 # Création de la figure avec des sous-graphiques
-fig, axs = plt.subplots(ncols=4, nrows=2, figsize=(20, 10))
+fig, axs = plt.subplots(ncols=4, nrows=nb_row, figsize=(20, 10))
 axs = axs.flatten()
 
 index = 0
